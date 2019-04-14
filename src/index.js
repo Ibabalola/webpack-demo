2e17b2f11328ca9dd7011e4470c52b2854ab0db9
Ibabalola <info@isaacbabalola.co.uk>
2019-04-14 12:50:33 +0100

    asset management complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+
 function component() {
 	let element = document.createElement('div');
 
-	element.innerHTML = ['Hello', 'Webpack'].join(' ');
+	let arr = ['Hello', 'Webpack'];
+
+	// add information garnered from data
+	let notes = Data.note;
+
+	console.log(notes);
+	Object.values(notes).forEach(value => {
+  		arr.push(value[0]);
+	})
+
+	element.innerHTML = arr.join(' ');
+	element.classList.add('hello');
+
+	// add the image to our existing div
+	let myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon); 
 
 	return element;
 }
