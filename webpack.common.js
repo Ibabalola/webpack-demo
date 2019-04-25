535c324c6bd0bb65c206922b1e2906a74e9b1b9e
isaac.babalola <isaac.babalola@playtech.com>
2019-04-25 12:37:01 +0100

    add production mode

--- webpack.common.js
+++ webpack.common.js
@@ -3,24 +3,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	mode: "development",
   	entry: {
      	app: './src/index.js',
-     	print: './src/print.js'
    	},
   	plugins: [
     	new CleanWebpackPlugin(),
     	new HtmlWebpackPlugin({
-       		title: 'Output Management'
+       		title: 'Production'
       	})
   	],
-  	devtool: 'inline-source-map',
-  	devServer: {
-  		contentBase: './dist'
-  	},
   	output: {
    		filename: '[name].bundle.js',
-   		path: path.resolve(__dirname, 'dist'),
-   		publicPath: '/'
+   		path: path.resolve(__dirname, 'dist')
   	}
 };
